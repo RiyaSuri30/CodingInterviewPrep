42647dfe1aebf59721a2be280bd5303a1a93ee50
RiyaSuri30 <85056245+RiyaSuri30@users.noreply.github.com>
2021-05-30 17:12:14 +0530

    Create CheckIfAnagram.java
    https://leetcode.com/problems/valid-anagram/submissions/

---
+++ CheckIfAnagram.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        //
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        int[] count = new int[26];
+        
+        for(int i=0;i<count.length;i++)
+        {
+            count[i]=0;
+        }
+        
+        // array count 
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<t.length();i++)
+        {
+            count[t.charAt(i)-'a']--;
+        }
+        
+        for(int i=0;i<count.length;i++)
+        {
+            if(count[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
