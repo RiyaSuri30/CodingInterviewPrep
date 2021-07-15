f060a26ea2cb52d6cb211bcbfcae3ad7e4c65c6b
RiyaSuri30 <85056245+RiyaSuri30@users.noreply.github.com>
2021-07-15 13:46:19 +0530

    Update 14.NumberOf1Bits.java

--- 14.NumberOf1Bits.java
+++ 14.NumberOf1Bits.java
@@ -12,3 +12,14 @@ public class Solution
         return count;
     }
 }
+
+// EXPLANATION :
+/* 
+
+
+For understanding the concepts of the working of this algorithm we work as follows
+that every number and the number less than that always have the last bits to be opposite in nature and thus that 
+that is after the occurrence of the first one all the remaining bits are complement of each other.
+so this helps to keep the track of the number of 1 by removing the ones to the end in every single iteration
+
+*/
